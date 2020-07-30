cb2ce7f9bc70eaeed6517a0316209893db285b50
Jak Ashuraliev <ashuraliev@gmail.com>
2020-07-30 11:04:39 -0700

    Chage ports for frontend and backend and build jar file

--- backend/src/main/java/com/codelabs/backend/api/TransactionRestContoller.java
+++ backend/src/main/java/com/codelabs/backend/api/TransactionRestContoller.java
@@ -4,11 +4,12 @@ import com.codelabs.backend.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
+@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:8001" })
 @RestController
 public class TransactionRestContoller {
 
--- frontend/src/service/BookDataService.jsx
+++ frontend/src/service/BookDataService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080";
+const API_URL = "http://localhost:8001";
 
 class BookDataService {
   //GET all books
